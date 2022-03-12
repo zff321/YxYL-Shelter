c8edfe2592e728e27f358971de44e8db499acc8d
YxYL6125 <1960378531@qq.com>
2022-03-12 19:51:49 +0800

    add sql file
    add some test data

---
+++ Sql/user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user`(`u_id`, `u_username`, `u_password`, `u_gender`, `u_address`, `u_tele`, `u_email`) VALUES (1, 'yxyl', '6125', '男', '重庆', '110', 'yxyl@ggmailc.pm');
+INSERT INTO `user`(`u_id`, `u_username`, `u_password`, `u_gender`, `u_address`, `u_tele`, `u_email`) VALUES (2, 'shelter', 'jialiang123', '男', NULL, '123123', 'shelter@jiba.com');
