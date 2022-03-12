56c65850d529d43cf463db989af3d89fa8b6d308
Makise <15351269850@163.com>
2022-03-12 14:10:21 +0800

    Update User.java
    set & get

--- src/main/java/com/yxyl_shelter/domain/User.java
+++ src/main/java/com/yxyl_shelter/domain/User.java
@@ -2,7 +2,67 @@ package com.yxyl_shelter.domain;
 
 public class User {
     private int u_id;
-    private int u_id;
-    
+    private String u_username;
+    private String u_password;
+    private String u_gender;
+    private String u_address;
+    private String u_tele;
+    private String u_eamil;
 
+
+    public int getU_id() {
+        return u_id;
+    }
+
+    public void setU_id(int u_id) {
+        this.u_id = u_id;
+    }
+
+    public String getU_username() {
+        return u_username;
+    }
+
+    public void setU_username(String u_username) {
+        this.u_username = u_username;
+    }
+
+    public String getU_password() {
+        return u_password;
+    }
+
+    public void setU_password(String u_password) {
+        this.u_password = u_password;
+    }
+
+    public String getU_gender() {
+        return u_gender;
+    }
+
+    public void setU_gender(String u_gender) {
+        this.u_gender = u_gender;
+    }
+
+    public String getU_address() {
+        return u_address;
+    }
+
+    public void setU_address(String u_address) {
+        this.u_address = u_address;
+    }
+
+    public String getU_tele() {
+        return u_tele;
+    }
+
+    public void setU_tele(String u_tele) {
+        this.u_tele = u_tele;
+    }
+
+    public String getU_eamil() {
+        return u_eamil;
+    }
+
+    public void setU_eamil(String u_eamil) {
+        this.u_eamil = u_eamil;
+    }
 }
